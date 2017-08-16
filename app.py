57d6bc1a9f227bc651dbb7e6cafcb93023ec27c4
Martin Pettersson <kontakt@martinpettersson.se>
2017-08-16 18:58:44 +0200

    Simple get and post methods.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,34 @@ tasks = [
         'done': False
     }
 ]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks' : tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task' : task[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id' : tasks[-1]['id'] + 1,
+        'title' : request.json['title'],
+        'description' : request.json.get('description', ""),
+        'done' : False
+    }
+    tasks.append(task)
+    return jsonify({'task' : task}), 201
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error' : 'Not Found'}), 404)
 
 @app.route('/')
 
