a483ee0c059e311a9a04d3d9f4513ad1234300af
Martin Pettersson <kontakt@martinpettersson.se>
2017-08-17 16:50:00 +0200

    Refactored.

---
+++ frontend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace frontend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
